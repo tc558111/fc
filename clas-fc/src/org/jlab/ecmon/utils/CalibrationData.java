d508336130daec6f4099a9237a32bfa9a53cef81
Cole Smith <lcs1956@me.com>
2015-12-30 14:11:28 -0500

    Fit Stabilizing
    Numerous edits to prevent exceptions and stabilize the fitting and
    display of fits.

--- clas-fc/src/org/jlab/ecmon/utils/CalibrationData.java
+++ clas-fc/src/org/jlab/ecmon/utils/CalibrationData.java
@@ -13,7 +13,8 @@ public class CalibrationData {
 	
     DetectorDescriptor desc = new DetectorDescriptor();
     
-    private List<GraphErrors>  graphs     = new ArrayList<GraphErrors>();
+    private List<GraphErrors>  rawgraphs  = new ArrayList<GraphErrors>();
+    private List<GraphErrors>  fitgraphs  = new ArrayList<GraphErrors>();
     private List<F1D>          functions  = new ArrayList<F1D>();
     private List<Double>  chi2            = new ArrayList<Double>(); 
     private int dataSize; 
@@ -25,70 +26,97 @@ public class CalibrationData {
     public DetectorDescriptor getDescriptor(){ return this.desc;}
     
     public void addGraph(double[] data, double[] error){
+    	
+    	GraphErrors graph;
 		String otab[]={"Ui Strip","Vi Strip","Wi Strip","Uo Strip","Vo Strip","Wo Strip"};
 		
 		dataSize = data.length;
-		int min=2 , max=dataSize-2;
+		int n, min=2 , max=dataSize-2;
+		
 		if (dataSize==1) {min=0 ; max=dataSize;}
 		if (dataSize==3) {min=0 ; max=dataSize;}
 		if (dataSize==5) {min=0 ; max=dataSize;}
 		if (dataSize==7) {min=1 ; max=dataSize-1;}
-		int size = max-min;
-        double[] xp  = new double[size];
-        double[] yp  = new double[size]; 
-        double[] xpe = new double[size];
-        double[] ype = new double[size]; 
-        int n=0;
-        for(int loop = min; loop < max; loop++){
-        	if (data[loop]>20) {
-        		xp[n]  = loop; 
-        		xpe[n] = 0.;
-        		yp[n]  = data[loop];
-        		ype[n] = error[loop];
+		
+        double[] xpraw  = new double[dataSize];
+        double[] ypraw  = new double[dataSize]; 
+        double[] xprawe = new double[dataSize];
+        double[] yprawe = new double[dataSize]; 
+ 
+        // For raw graph
+        n=0;
+        for(int loop = 0; loop < data.length; loop++) {
+        	if(loop>=min&&loop<max&&data[loop]>20) n++;        		
+    		xpraw[loop]  = loop; 
+    		xprawe[loop] = 0.;
+    		ypraw[loop]  = data[loop];
+    		yprawe[loop] = Math.max(0.1,error[loop]);        	
+        }
+        
+        double[] xpfit  = new double[n];
+        double[] ypfit  = new double[n]; 
+        double[] xpfite = new double[n];
+        double[] ypfite = new double[n];  
+        
+        // For fit graph
+        n=0;
+        for(int loop = 0; loop < data.length; loop++){
+        	if(loop>=min&&loop<max&&data[loop]>20) {      		
+         		xpfit[n]  = xpraw[loop]; 
+        		xpfite[n] = xprawe[loop];
+        		ypfit[n]  = ypraw[loop];
+        		ypfite[n] = Math.max(0.1,yprawe[loop]);
         		n++;
         	}
         }
 
-        GraphErrors  graph = new GraphErrors(xp,yp,xpe,ype);   
+        graph = new GraphErrors(xpfit,ypfit,xpfite,ypfite);   
         graph.setXTitle("Pixel Number");
         graph.setYTitle("Mean ADC");
         graph.setMarkerStyle(2);
         graph.setMarkerSize(8);
+        
         int sector=getDescriptor().getSector()+1;
         int   view=getDescriptor().getLayer();
         int  strip=getDescriptor().getComponent()+1;
-        graph.setTitle("EXP FIT: Sector "+sector+" "+otab[view-1]+""+strip);
-        this.graphs.add(graph);
+        
+        graph.setTitle("EXP FIT: Sector "+sector+" "+otab[view-1]+""+strip);        
+        this.fitgraphs.add(graph);
+        
+        graph = new GraphErrors(xpraw,ypraw,xprawe,yprawe);   
+        graph.setMarkerColor(2);
+        graph.setMarkerStyle(2);  graph.setMarkerSize(6); graph.setMarkerColor(4);
+
+        this.rawgraphs.add(graph);
+        
         F1D f1 = new F1D("exp",0,max);
         this.functions.add(f1);
     }
     
     public void analyze(){
     	DataFitter.FITPRINTOUT=false;
-        for(int loop = 0; loop < this.graphs.size(); loop++){
+        for(int loop = 0; loop < this.fitgraphs.size(); loop++){
             F1D func = this.functions.get(loop);
             func.setParameter(0,0.);
             func.setParameter(1,0.);
-            double [] dataY=this.graphs.get(loop).getDataY().getArray();
-            //double [] dataX=this.graphs.get(loop).getDataX().getArray();
+            double [] dataY=this.fitgraphs.get(loop).getDataY().getArray();
             if (dataY.length>0) {
             	int imax = Math.min(2,dataY.length-1);
             	double p0try = dataY[imax] ; double p0min = p0try-30. ; double p0max=p0try+30.;
             	func.setParameter(0, p0try);
             	func.setParameter(1,-0.0144);
             	func.setParLimits(0,p0min,p0max);
-            	//func.setParLimits(1,-0.03,-0.014);
-            	func.setParLimits(1,-0.04,-0.001);
+            	func.setParLimits(1,-0.5,-0.001);
             	func.setLineColor(2);
-            	this.graphs.get(loop).fit(this.functions.get(loop));
-            	chi2.add(DataFitter.getChiSquareFunc(this.graphs.get(loop),func));
-            	//chi2.add(func.getChiSquare(func.getDataSet(func.getDataSize())));
+            	this.fitgraphs.get(loop).fit(this.functions.get(loop));	//Fit data
+            	this.chi2.add(DataFitter.getChiSquareFunc(this.fitgraphs.get(loop),func));
             }
         }
         
     }
     
-    public GraphErrors  getGraph(int index){return this.graphs.get(index);}
+    public GraphErrors  getFitGraph(int index){return this.fitgraphs.get(index);}
+    public GraphErrors  getRawGraph(int index){return this.rawgraphs.get(index);}
     public F1D          getFunc(int index) {return this.functions.get(index);}
-    public double       getChi2(int index) {return this.chi2.get(index);}
+    public double       getChi2(int index) {if (this.chi2.isEmpty()==false) return this.chi2.get(index); else return 0.;}
 }

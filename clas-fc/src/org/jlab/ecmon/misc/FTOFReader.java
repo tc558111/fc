7b800753fcfb8669f8f061f70517562b94bdc0f0
lsmith <lsmith@192.168.1.106>
2016-02-05 00:34:35 -0500

    Changed Multiplicity Test
    Extra TDC hits in 1B are failing default multiplicity test so I
    uncommented the manual test and removed requirement of 1 TDC hit.

--- clas-fc/src/org/jlab/ecmon/misc/FTOFReader.java
+++ clas-fc/src/org/jlab/ecmon/misc/FTOFReader.java
@@ -20,22 +20,28 @@ import org.root.pad.TGCanvas;
 public class FTOFReader {
 		
 	FADCConfigLoader          fadcLoader  = new FADCConfigLoader();
+	H2D hADCPADDLE,hTDCPADDLE;
+	
 	FTOFReader(){				
 		}
 		
-		public void plot() {
+		public void plot(int panel) {
+	      String plt[] = {"A","B"};
+	      int nsab[] = {9,19};
 		  int icounter = 0;
-          String input = "/Users/colesmith/CLAS/crt/gemc/sector2_000257_mode7.evio";
+          String input = "/Users/colesmith/coatjava/dat/sector2_000257_mode7.evio";
 	      EvioSource  reader = new EvioSource();
 	      reader.open(input);
 	      EventDecoder decoder = new EventDecoder();
-	      H1D hADC = new H1D("hADC",60,0.0,3000.0);
+	      H1D hADC = new H1D("hADC",60,0.0,5000.0);
 	      H1D hTDC = new H1D("hTDC",100,-2000.0,2000.0);
-	      H2D hADCPADDLE = new H2D("hADCPADDLE",60,    0.0,3000.0,62,0.5,62.5);
-	      H2D hTDCPADDLE = new H2D("hTDCPADDLE",40,-1000.0,1000.0,62,0.5,62.5);
-	      //H2D hADCPADDLE = new H2D("hADCPADDLE",60,    0.0,3000.0,23,0.5,23.5);
-	      //H2D hTDCPADDLE = new H2D("hTDCPADDLE",40,-1000.0,1000.0,23,0.5,23.5);
-
+	      if (panel==1) {
+	        hADCPADDLE = new H2D("hADCPADDLE",60,    0.0,5000.0,62,0.5,62.5);
+	        hTDCPADDLE = new H2D("hTDCPADDLE",40,-1000.0,1000.0,62,0.5,62.5);
+	      }else{
+	        hADCPADDLE = new H2D("hADCPADDLE",60,    0.0,3000.0,23,0.5,23.5);
+	        hTDCPADDLE = new H2D("hTDCPADDLE",40,-1000.0,1000.0,23,0.5,23.5);
+	      }
 	      hADC.setLineWidth(2);
 	      hADC.setFillColor(3);
 	      hTDC.setLineWidth(2);
@@ -43,8 +49,8 @@ public class FTOFReader {
 
 	      hADC.setXTitle("SQRT(ADCL*ADCR)");
 	      hTDC.setXTitle("TDCL-TDCR");
-	      hADCPADDLE.setYTitle("FTOF1A PADDLE #");
-	      hTDCPADDLE.setYTitle("FTOF1A PADDLE #");
+	      hADCPADDLE.setYTitle("FTOF1"+plt[panel]+" PADDLE #");
+	      hTDCPADDLE.setYTitle("FTOF1"+plt[panel]+" PADDLE #");
 	      hADCPADDLE.setXTitle("SQRT(ADCL*ADCR)");
 	      hTDCPADDLE.setXTitle("TDCL-TDCR");
 
@@ -52,13 +58,19 @@ public class FTOFReader {
 	        icounter++;
 	        EvioDataEvent event = (EvioDataEvent) reader.getNextEvent();	        
 	        decoder.decode(event);
-	        /*
-            List<DetectorBankEntry> strips = decoder.getDataEntries("FTOF1B");
-            System.out.println(" ");
+	        
+	        List<DetectorBankEntry> strips;
+	        
+            if (panel==1){	
+              strips = decoder.getDataEntries("FTOF1B");
+            }else{
+              strips = decoder.getDataEntries("FTOF1A");
+            }	
+            
             int adcL=0,adcR=0,tdcL=0,tdcR=0;
             int adc,ped,nsb=0,nsa=0,npk=0,timf=0,timc=0;
             double pedref=0.,mc_t=0.,tdc=0,tdcf=0;
-            System.out.println(" ");
+            //System.out.println(" ");
             for(DetectorBankEntry strip : strips) {
                 adc=ped=npk=timf=timc=0 ; pedref=0.;
             	//System.out.println(strip);
@@ -73,55 +85,63 @@ public class FTOFReader {
             	if(strip.getType()==BankType.TDC) {
             		int[] tdcc = (int[]) strip.getDataObject();
             		tdc = tdcc[0]*24./1000.;
+            		//if(is==2) {System.out.println("pmt,order,tdc= "+ip+" "+iord+" "+tdcc[0]);}
             	}
             	if(strip.getType()==BankType.ADCFPGA) {
             		int[] adcc= (int[]) strip.getDataObject();
             		ped = adcc[2];
             		npk = adcc[3];
-            		adc = (adcc[1]-ped*19);
+            		adc = (adcc[1]-ped*nsab[panel]);
             		timf = DataUtils.getInteger(adcc[0],0,5);
             		timc = DataUtils.getInteger(adcc[0],6,14);
             		tdcf = timc*4.+timf*0.0625;
             		FADCConfig config=fadcLoader.getMap().get(icr,isl,ich);
-            		nsa = config.getNSA();
-            		nsb = config.getNSB();
-            		pedref = config.getPedestal();
+            		//nsa = config.getNSA();
+            		//nsb = config.getNSB();
+            		//pedref = config.getPedestal();
+            		//if(is==2) {System.out.println("pmt,order,adc= "+ip+" "+iord+" "+adc);}            		
             	}            	
-                System.out.println("crate,slot,chan:"+icr+" "+isl+" "+ich);
-    			System.out.println("sector,layer,pmt,order"+is+" "+il+" "+ip+" "+iord);
-    			System.out.println("  nchan,tdc,adc,ped,pedref,nsb,nsa: "+npk+" "+tdc+" "+adc+" "+ped+" "+pedref+" "+nsb+" "+nsa);
+                //System.out.println("crate,slot,chan:"+icr+" "+isl+" "+ich);
+    			//System.out.println("sector,layer,pmt,order"+is+" "+il+" "+ip+" "+iord);
+    			//System.out.println("  nchan,tdc,adc,ped,pedref,nsb,nsa: "+npk+" "+tdc+" "+adc+" "+ped+" "+pedref+" "+nsb+" "+nsa);
             }
             
-            */	
             	
-            List<DetectorCounter> banks = decoder.getDetectorCounters(DetectorType.FTOF1A);
-
+	        List<DetectorCounter> banks;
+            if (panel==1){	
+              banks = decoder.getDetectorCounters(DetectorType.FTOF1B);
+            }else{
+              banks = decoder.getDetectorCounters(DetectorType.FTOF1A);
+            }
 	        for(DetectorCounter bank : banks){
 	          if(bank.getChannels().size()==2){
-	             if(bank.isMultiHit()==false){
+//	             if(bank.isMultiHit()==false){
 	               // isMultihit() method returns false when
-	               //  (bank.getChannels().get(0).getADC().size()==1&&
-	               //   bank.getChannels().get(1).getADC().size()==1&&
-	               //   bank.getChannels().get(0).getTDC().size()==1&&
-	               //   bank.getChannels().get(1).getTDC().size()==1)
+	            	 
+	               if (bank.getChannels().get(0).getADC().size()==1&&
+	                   bank.getChannels().get(1).getADC().size()==1&&
+	                   bank.getChannels().get(0).getTDC().size()>0&&
+	                   bank.getChannels().get(1).getTDC().size()>0)
+	               {
 	               // it checks if each channel has one ADC and one TDC.
-	               int adcL = bank.getChannels().get(0).getADC().get(0);
-	               int adcR = bank.getChannels().get(1).getADC().get(0);
-	               int tdcL = bank.getChannels().get(0).getTDC().get(0);
-	               int tdcR = bank.getChannels().get(1).getTDC().get(0);
-                   hADC.fill(Math.sqrt(adcL*adcR));
-                   hTDC.fill(tdcL-tdcR);	     
+	               int aadcL = bank.getChannels().get(0).getADC().get(0);
+	               int aadcR = bank.getChannels().get(1).getADC().get(0);
+	               int ttdcL = bank.getChannels().get(0).getTDC().get(0);
+	               int ttdcR = bank.getChannels().get(1).getTDC().get(0);
+                   hADC.fill(Math.sqrt(aadcL*aadcR));
+                   hTDC.fill(ttdcL-ttdcR);	     
 	               int paddle = bank.getDescriptor().getComponent();
-	               
-                   hADCPADDLE.fill(Math.sqrt(adcL*adcR),paddle);
-	               hTDCPADDLE.fill(tdcL-tdcR,paddle);
-	             }
+	               //System.out.println("paddle,adcL,R,tdcL,R= "+paddle+" "+aadcL+" "+aadcR+" "+ttdcL+" "+ttdcR);
+                   hADCPADDLE.fill(Math.sqrt(aadcL*aadcR),paddle);
+	               hTDCPADDLE.fill(ttdcL-ttdcR,paddle);
+	               }
+//	             }
 	          }
 	        }
 	        
 	        
 	      }
-	      TGCanvas c1 = new TGCanvas("c1","FTOF1B",1200,800,2,2);
+	      TGCanvas c1 = new TGCanvas("c1","FTOF",1200,800,2,2);
 	      c1.cd(0);
 	      c1.draw(hADC);
 	      c1.cd(1);
@@ -136,7 +156,8 @@ public class FTOFReader {
 	
 	public static void main(String[] args) {		
 		FTOFReader tof = new FTOFReader();
-		tof.plot();
+		tof.plot(0);
+		tof.plot(1);
 	}
 	
 }

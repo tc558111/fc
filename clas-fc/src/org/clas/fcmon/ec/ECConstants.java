3ab006931f5023f14c2410c70ec9362c883053b6
Cole Smith <lcs1956@me.com>
2016-10-05 16:32:46 -0400

    New checkin

---
+++ clas-fc/src/org/clas/fcmon/ec/ECConstants.java
@@ -0,0 +1,12 @@
+package org.clas.fcmon.ec;
+
+public class ECConstants {
+    
+    public static int IS1 = 2 ;
+    public static int IS2 = 3 ;  
+    
+    public static final void setSectors(int is1, int is2) {
+        IS1=is1;
+        IS2=is2;
+    }
+}
